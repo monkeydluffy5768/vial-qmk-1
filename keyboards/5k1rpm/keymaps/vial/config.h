04af2243bd5824b31a533e84300270cd14362f26
monkeydluffy5768 <saiyo5768@gmail.com>
2023-03-15 17:58:43 +0530

    u5

---
+++ keyboards/5k1rpm/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x6F, 0xD2, 0x03, 0xC4, 0x16, 0xAF, 0x61, 0x27}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 1}
+#define VIAL_UNLOCK_COMBO_COLS {0, 1}
